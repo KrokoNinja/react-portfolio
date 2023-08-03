f21fe3fbbc48b8d6f4b109983d4f4e5942b3da50
KrokoNinja <79910460+KrokoNinja@users.noreply.github.com>
2023-08-03 19:56:26 +0200

    style sidebar, add tags style for layout component

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,28 +1,22 @@
 import './index.scss'
 import { Link, NavLink } from 'react-router-dom'
-import LogoS from '../../assets/images/logo-s.png'
+import LogoS from '../../assets/images/cut-out-lennert.jpg'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faEnvelope,
   faHome,
   faMailBulk,
-  faMailForward,
-  faMailReply,
   faUser,
 } from '@fortawesome/free-solid-svg-icons'
-import {
-  faGithub,
-  faLinkedin,
-  faMailchimp,
-} from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => {
   return (
     <div className="nav-bar">
       <Link className="logo" to="/">
         <img src={LogoS} alt="logo" />
-        <img src={LogoSubtitle} alt="slobodan" className="sub-logo" />
+        <p className="sub-logo">Lennert</p>
       </Link>
       <nav>
         <NavLink exact="true" to="/" activeClassName="active">
