026acfc118814690c7b5701f4dc8ad7592fa9e43
KrokoNinja <79910460+KrokoNinja@users.noreply.github.com>
2023-08-04 19:23:06 +0200

    add letter styling and animation

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,11 @@
 import { Link } from 'react-router-dom'
 import './index.scss'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
 
 const Home = () => {
-  const [letterClass, setLetterClass] = useState('letter')
-  const nameArray = ['L', 'e', 'n', 'n', 'e', 'r', 't']
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = [' ', 'L', 'e', 'n', 'n', 'e', 'r', 't', ',']
   const jobArray = [
     'F',
     'r',
@@ -26,6 +26,28 @@ const Home = () => {
     'e',
     'r',
   ]
+  const countryArray = [
+    'f',
+    'r',
+    'o',
+    'm',
+    ' ',
+    'G',
+    'e',
+    'r',
+    'm',
+    'a',
+    'n',
+    'y',
+    '.',
+  ]
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 6000)
+    return () => clearTimeout(timer)
+  }, [])
 
   return (
     <div className="container home-page">
@@ -35,19 +57,24 @@ const Home = () => {
           <span className={`${letterClass} _12`}>i,</span>
           <br />
           <span className={`${letterClass} _13`}>I</span>
-          <span className={`${letterClass} _14`}>'m </span>
+          <span className={`${letterClass} _14`}>'m</span>
           <AnimatedLetters
             letterClass={letterClass}
             strArray={nameArray}
             idx={15}
           />
-          , <br />
+          <br />
           <AnimatedLetters
             letterClass={letterClass}
             strArray={jobArray}
             idx={22}
           />
-          <br /> from Germany.
+          <br />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={countryArray}
+            idx={42}
+          />
         </h1>
         <h2>Student / learning Frontend</h2>
         <h3>
