0d19e13abc238d4c6a1abfc41cb82b9a7d9d5659
KrokoNinja <79910460+KrokoNinja@users.noreply.github.com>
2023-08-08 15:08:06 +0200

    add content to Contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,11 +1,79 @@
 import Loader from 'react-loaders'
 import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
+import { useEffect, useState } from 'react'
 
 const Contact = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+    return () => clearTimeout(timer)
+  }, [])
+
   return (
     <>
       <div className="container contact-page">
-        <h1>Contact</h1>
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'M', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            Thank you for visiting my portfolio website. If you have any
+            questions, inquiries, or just want to chat about all things frontend
+            development, I'd love to hear from you. Whether you're interested in
+            collaborating on a project, seeking advice, or simply want to
+            connect with fellow tech enthusiasts, I'm here and ready to engage.
+          </p>
+          <p>
+            Feel free to reach out using the contact form below or connect with
+            me through my social media profiles. I'm always excited to discuss
+            new ideas, explore innovative technologies, and contribute to the
+            ever-evolving world of web development.
+          </p>
+          <p>Looking forward to connecting with you!</p>
+          <div className="contact-form">
+            <form>
+              <ul>
+                <li className="half">
+                  <input type="text" name="name" placeholder="Name" required />
+                </li>
+                <li className="half">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    type="text"
+                    name="subject"
+                    placeholder="Subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    name="message"
+                    placeholder="Message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" value="SEND" className="flat-button" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
       </div>
       <Loader type="pacman" />
     </>
